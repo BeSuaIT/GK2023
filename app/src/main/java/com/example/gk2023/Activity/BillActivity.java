f3e1dc52b7cc4149eb309e86155d282886e1cc0b
Bé Sữa <hoangluuminh1@gmail.com>
2024-03-25 14:39:48 +0700

    Cart Activity

---
+++ app/src/main/java/com/example/gk2023/Activity/BillActivity.java
@@ -0,0 +1,16 @@
+package com.example.gk2023.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.gk2023.R;
+
+public class BillActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bill);
+    }
+}
